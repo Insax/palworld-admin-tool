7f576627cb330a55cfffeeb970d890932952dbb9
Insax <m.kram@live.at>
2024-02-10 00:55:44 +0100

    Fixes

--- app/Livewire/ServerOverview.php
+++ app/Livewire/ServerOverview.php
@@ -23,7 +23,7 @@ class ServerOverview extends Component
         $server->players()->delete();
         $server->delete();
         Toaster::success('Server has been successfully deleted');
-        $this->servers = Server::with('rconData')->get();
+        return redirect()->route('server-overview');
     }
 
     public function toggleActiveServer(Server $server)
